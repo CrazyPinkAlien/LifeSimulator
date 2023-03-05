6d6fdf89c7a5cf9913a5ed09f77a14543ef4be97
Emily Tyler <emily.et@live.com>
2023-03-05 17:38:25 +0000

    Changed ui to egui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::app::App;
 use bevy::DefaultPlugins;
+use bevy_egui::EguiPlugin;
 
 use crate::ui::UIPlugin;
 use crate::person::player::setup_player;
@@ -7,18 +8,10 @@ use crate::person::player::setup_player;
 pub mod person;
 pub mod ui;
 
-// States
-#[derive(Clone, Hash, Debug, Eq, PartialEq)]
-enum AppState {
-    MainMenu,
-    PlayerInfo,
-}
-
 fn main() {
     App::new()
-        .add_state(AppState::MainMenu)
-
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_plugin(UIPlugin)
 
         .add_startup_system(setup_player)
--- src/ui.rs
+++ src/ui.rs
@@ -1,16 +1,24 @@
-//UI functonality
+// UI functonality
+use bevy::app::{App, Plugin};
+use bevy::ecs::system::{ResMut, Res};
+use bevy::prelude::{Resource, With, Query, IntoSystemDescriptor};
+use bevy_egui::EguiContext;
+use bevy_egui::egui::{CentralPanel, SidePanel, DragValue};
+use bevy_egui::egui::widgets::Button;
 
-use bevy::prelude::{Plugin, App, Commands, AssetServer, Res, Camera2dBundle, Component, Query, With, Changed, Color, default, BuildChildren};
-use bevy::text::{Text, TextStyle};
-use bevy::ui::{Style, Size, Val, UiRect, Interaction, BackgroundColor, JustifyContent, AlignItems};
-use bevy::ui::node_bundles::{ButtonBundle, TextBundle};
-use bevy::ui::widget::Button;
-use bevy::ecs::schedule::{SystemSet, State};
-use bevy::ecs::system::ResMut;
-
-use crate::AppState;
 use crate::person::{Name, Age};
 use crate::person::player::Player;
+
+// UI States
+#[derive(PartialEq)]
+enum UIState {
+    MainMenu,
+    PlayerInfo,
+}
+
+// Current UI state
+#[derive(Resource, PartialEq)]
+struct CurrentUIState(UIState);
 
 // Create UI plugin
 pub struct UIPlugin;
@@ -18,119 +26,57 @@ pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_startup_system(setup_ui)
-        .add_startup_system(setup_player_name)
-
-        .add_system(update_player_name)
-        .add_system(button_system)
-        .add_system(player_name_button)
-
-        .add_system_set(
-            SystemSet::on_update(AppState::PlayerInfo)
-                .with_system(display_player_age)
-        );
+            .insert_resource(CurrentUIState(UIState::MainMenu))
+            .add_system(left_side_menu)
+            .add_system(main_menu_ui.after(left_side_menu))
+            .add_system(player_info_ui.after(left_side_menu));
     }
 }
 
-// Function to setup the UI
-fn setup_ui(mut commands: Commands) {
-    // UI camera
-    commands.spawn(Camera2dBundle::default());
-}
-
-#[derive(Component)]
-struct PlayerNameText;
-
-// Function to write the player name at the top of the screen
-fn setup_player_name(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn( ButtonBundle {
-        style: Style {
-            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-            // center button
-            margin: UiRect::all(Val::Auto),
-            // horizontally center child text
-            justify_content: JustifyContent::Center,
-            // vertically center child text
-            align_items: AlignItems::Center,
-            ..default()
-        },
-        background_color: NORMAL_BUTTON.into(),
-        ..default()
-    })
-    .with_children(|parent| {
-        parent.spawn((TextBundle::from_section(
-            "",
-            TextStyle {
-                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                font_size: 40.0,
-                color: Color::rgb(0.9, 0.9, 0.9),
-            }
-        ), PlayerNameText));
+// Left hand side menu
+fn left_side_menu(mut ui_state: ResMut<CurrentUIState>, mut egui_context: ResMut<EguiContext>) {
+    SidePanel::left("side_panel").default_width(200.0).show(egui_context.ctx_mut(), |ui|  {
+        if ui.add(Button::new("Main Menu")).clicked() {
+            ui_state.0 = UIState::MainMenu;
+        }
+        if ui.add(Button::new("Player Info")).clicked() {
+            ui_state.0 = UIState::PlayerInfo;
+        }
     });
 }
 
-// Function to update the player name whereever it is used
-fn update_player_name(player_query: Query<&Name, (Changed<Name>, With<Player>)>, mut text_query: Query<&mut Text, With<PlayerNameText>>) {
-    if !player_query.is_empty() {
-        // Get player name
-        let player_name = player_query.single();
-        // Update text
-        for mut text in &mut text_query.iter_mut() {
-            text.sections[0].value = player_name.first.clone();
-        }
+// UI for the main menu
+fn main_menu_ui(ui_state: Res<CurrentUIState>, mut egui_context: ResMut<EguiContext>, player_query: Query<(&Name, &Age), With<Player>>) {
+    if ui_state.0 == UIState::MainMenu && !player_query.is_empty() {
+        // Get player info
+        let player_info = player_query.single();
+        let player_name = player_info.0;
+        let player_age = player_info.1;
+        // Draw UI
+        CentralPanel::default().show(egui_context.ctx_mut(), |ui| {
+            ui.heading("Main Menu");
+            ui.label(format!("{} {}", player_name.first.clone(), player_name.last.clone()));
+            ui.label(format!("Age: {}", player_age.age.to_string()));
+        });
     }
 }
 
-// Player name button pressed
-fn player_name_button(mut interaction_query: Query<&Interaction,(Changed<Interaction>, With<Button>, With<PlayerNameText>)>, mut app_state: ResMut<State<AppState>>) {
-    if !interaction_query.is_empty() {
-        let interaction = interaction_query.single_mut();
-        if *interaction == Interaction::Clicked {
-            app_state.set(AppState::PlayerInfo).unwrap();
-        }
-    }
-}
-
-// Button colours definitions
-const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
-
-// Colour system for all buttons
-fn button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor),
-        (Changed<Interaction>, With<Button>),
-    >
-) {
-    for (interaction, mut color) in &mut interaction_query {
-        match *interaction {
-            Interaction::Clicked => {
-                *color = PRESSED_BUTTON.into();
-            }
-            Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-            }
-            Interaction::None => {
-                *color = NORMAL_BUTTON.into();
-            }
-        }
-    }
-}
-
-#[derive(Component)]
-struct PlayerAgeText;
-
-fn display_player_age(mut commands: Commands, asset_server: Res<AssetServer>, age_query: Query<&Age, With<Player>>) {
-    if !age_query.is_empty() {
-        let age = age_query.single();
-        commands.spawn((TextBundle::from_section(
-            age.age.to_string(),
-            TextStyle {
-                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                font_size: 100.0,
-                color: Color::rgb(0.9, 0.9, 0.9),
-            }
-        ), PlayerAgeText));
+// UI showing player info
+fn player_info_ui(ui_state: Res<CurrentUIState>, mut egui_context: ResMut<EguiContext>, mut player_query: Query<(&mut Name, &mut Age), With<Player>>) {
+    if ui_state.0 == UIState::PlayerInfo && !player_query.is_empty() {
+        // Get player info
+        let player_info = player_query.single_mut();
+        let mut player_name = player_info.0;
+        let mut player_age = player_info.1;
+        // Draw UI
+        CentralPanel::default().show(egui_context.ctx_mut(), |ui| {
+            ui.heading("Player Info");
+            ui.label("First Name:");
+            ui.text_edit_singleline(&mut player_name.first);
+            ui.label("Last Name:");
+            ui.text_edit_singleline(&mut player_name.last);
+            ui.label("Age:");
+            ui.add(DragValue::new(&mut player_age.age));
+        });
     }
 }
